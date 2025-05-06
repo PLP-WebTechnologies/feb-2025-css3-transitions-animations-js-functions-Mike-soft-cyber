113ecd44c2c240b4dac400860e5d04761bfb891a
Michael-kariuki <kariukimichaelw@gmail.com>
2025-05-06 19:32:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.querySelector('.anim');
+        button.addEventListener('click', () => {
+            document.body.style.animation = 'changeBackground 2s infinite';
+        });
